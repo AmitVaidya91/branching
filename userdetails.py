131d26d77b43dfd52b41e5a13fa8ff6c304e689a
Obama1 <vaidyaamit2008@gmail.com>
2026-09-01 09:50:42 +0530

    saving the file into the main branch

---
+++ userdetails.py
@@ -0,0 +1,5 @@
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+city = input("Enter your city: ")
+
+print("Your details: ",firstName+" "+lastName+" "+city)
